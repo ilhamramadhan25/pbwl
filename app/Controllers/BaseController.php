06a3b09f4efc54752cbb5fb83fff7ce95999d751
ilhamramadhan25 <ilham.ramadhan.r25@gmail.com>
2026-06-09 13:50:10 +0700

    Add files via upload

---
+++ app/Controllers/BaseController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controllers;
+
+use CodeIgniter\Controller;
+use CodeIgniter\HTTP\RequestInterface;
+use CodeIgniter\HTTP\ResponseInterface;
+use Psr\Log\LoggerInterface;
+
+/**
+ * BaseController provides a convenient place for loading components
+ * and performing functions that are needed by all your controllers.
+ *
+ * Extend this class in any new controllers:
+ * ```
+ *     class Home extends BaseController
+ * ```
+ *
+ * For security, be sure to declare any new methods as protected or private.
+ */
+abstract class BaseController extends Controller
+{
+    protected $helpers = ['form', 'url'];
+
+    public function initController(
+        RequestInterface $request,
+        ResponseInterface $response,
+        LoggerInterface $logger
+    ) {
+        parent::initController($request, $response, $logger);
+    }
+}
